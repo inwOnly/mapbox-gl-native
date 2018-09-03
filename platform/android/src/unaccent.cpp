c0de0aa8161770f1d23734495e798beb2de25167
tobrun <tobrun.van.nuland@gmail.com>
2018-09-03 11:58:54 +0200

    [android] - add platform specific implementation of unaccent

---
+++ platform/android/src/unaccent.cpp
@@ -0,0 +1,12 @@
+#include <unaccent.hpp>
+
+namespace mbgl {
+namespace platform {
+
+std::string unaccent(const std::string& str) {
+    // TODO use Normalizer.java
+    return str;
+}
+
+} // namespace platform
+} // namespace mbgl
